5855365c3430fe342162dcb4d9a19c3f7f409fc5
Zhyvchyn Ihor <33670389+watergor@users.noreply.github.com>
2023-02-18 20:02:38 +0000

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+JOBS = [
+  {'id':1,
+   'title':'Front end',
+   'location':'Lviv,UA',
+   'salary':'$2000'
+  },
+  {'id':2,
+   'title':'Back end',
+   'location':'Horodok,UA',
+   'salary':'$3000'
+  },
+  {'id':3,
+   'title':'QA',
+   'location':'Remote',
+   'salary':'$1000'
+  },
+  {'id':4,
+   'title':'Manager',
+   'location':'Uhry,UA',
+   'salary':'$10 000'
+  },
+]
 @app.route("/")
 def HomePage():
-  return render_template('home.html')
+  return render_template('home.html',jobs = JOBS,my_name='Ігор Живчин')
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", debug = True)
